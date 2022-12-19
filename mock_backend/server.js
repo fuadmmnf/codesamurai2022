120bf6476b726f94e839addbf5a1f18dc3960341
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-12-19 23:21:27 +0600

    added mock backend

--- mock_backend/server.js
+++ mock_backend/server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
-const app = jsonServer.create();
+const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
 
-app.use(middlewares);
-app.use(router);
+server.use(middlewares);
+server.use(router);
 
-app.listen(port);
+server.listen(port);
