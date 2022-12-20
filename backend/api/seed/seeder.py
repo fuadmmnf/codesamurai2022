7a9a56d2af962b653f2187366b6bb3c59589e70a
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-12-20 21:59:20 +0600

    added project CRUD

--- backend/api/seed/seeder.py
+++ backend/api/seed/seeder.py
@@ -2,11 +2,11 @@ import pandas as pd
 from api.models import *
 from datetime import datetime
 
-roles = json.load(open('api/seed/data/user_types.csv'))
+roles = pd.read_csv(open('api/seed/data/user_types.csv'))
 UserType.objects.all().delete()
 User.objects.all().delete()
-for role in roles:
-    ut = UserType(
+for i, role in roles.iterrows():
+    UserType(
         code=role['code'],
         committee=role['committee'],
         description=role['description'],
@@ -15,73 +15,72 @@ for role in roles:
     User(
         username=role['code'],
         password=hash('secret123'),
-        user_type=ut,
+        user_type=UserType.objects.get(code__exact=role['code']),
     ).save()
 
-    agencies = json.load(open('api/seed/data/agencies.csv'))
-    Agency.objects.all().delete()
-    for agency in agencies:
-        Agency(
-            code=agency['code'],
-            name=agency['name'],
-            type=agency['type'],
-            description=agency['description'],
-        ).save()
+agencies = pd.read_csv(open('api/seed/data/agencies.csv'))
+Agency.objects.all().delete()
+for i, agency in agencies.iterrows():
+    Agency(
+        code=agency['code'],
+        name=agency['name'],
+        type=agency['type'],
+        description=agency['description'],
+    ).save()
 
-    projects = json.load(open('api/seed/data/projects.csv'))
-    Project.objects.all().delete()
-    for project in projects:
-        Project(
-            project_id=project['project_id'],
-            exec=Agency.objects.get(code__exact=project['exec']),
-            project_name=project['project_name'],
-            location=project['location'],
-            start_date=datetime.strptime(project['start_date'], "%Y-%m-%d"),
-            total_budget=project['total_budget'],
-            latitute=project['latitute'],
-            longitude=project['longitude'],
-            cost=project['cost'],
-            timespan=project['timespan'],
-            goal=project['goal'],
-            completion=project['completion'],
-            actual_cost=project['actual_cost'],
-        ).save()
+projects = pd.read_csv(open('api/seed/data/projects.csv'))
+Project.objects.all().delete()
+for i, project in projects.iterrows():
+    Project(
+        project_id=project['project_id'],
+        exec=Agency.objects.get(code__exact=project['exec']),
+        project_name=project['name'],
+        location=project['location'],
+        start_date=datetime.strptime(project['start_date'], "%Y-%m-%d"),
+        latitude=project['latitude'],
+        longitude=project['longitude'],
+        cost=project['cost'],
+        timespan=project['timespan'],
+        goal=project['goal'],
+        completion=project['completion'],
+        actual_cost=project['actual_cost'],
+    ).save()
 
-    components = json.load(open('api/seed/data/components.csv'))
-    Component.objects.all().delete()
-    for component in components:
-        Component(
-            component_id=component['component_id'],
-            project_id=Project.objects.get(project_id__exact=component['project_id']),
-            execution_agency=Agency.objects.get(code__exact=component['execution_agency']),
-            component_type=component['component_type'],
-            depends_on=None if len(component['depends_on']) < 1 else Component.objects.get(
-                component_id__exact=component['depends_on']),
-            budget_ratio=component['budget_ratio'],
-        ).save()
+components = pd.read_csv(open('api/seed/data/components.csv'))
+Component.objects.all().delete()
+for i, component in components.iterrows():
+    Component(
+        component_id=component['component_id'],
+        project_id=Project.objects.get(project_id__exact=component['project_id']),
+        exec=Agency.objects.get(code__exact=component['execution_agency']),
+        component_type=component['component_type'],
+        depends_on=None if len(component['depends_on']) < 1 else Component.objects.get(
+            component_id__exact=component['depends_on']),
+        budget_ratio=component['budget_ratio'],
+    ).save()
 
-    proposals = json.load(open('api/seed/data/proposals.csv'))
-    for proposal in proposals:
-        Project(
-            project_id=proposal['project_id'],
-            exec=Agency.objects.get(code__exact=proposal['exec']),
-            project_name=proposal['project_name'],
-            location=proposal['location'],
-            proposal_date=datetime.strptime(proposal['proposal_date'], "%Y-%m-%d"),
-            total_budget=proposal['total_budget'],
-            latitute=proposal['latitute'],
-            longitude=proposal['longitude'],
-            cost=proposal['cost'],
-            timespan=proposal['timespan'],
-            goal=proposal['goal'],
-            is_accepted=False,
-        ).save()
+proposals = pd.read_csv(open('api/seed/data/proposals.csv'))
+for i, proposal in proposals.iterrows():
+    Project(
+        project_id=proposal['project_id'],
+        exec=Agency.objects.get(code__exact=proposal['exec']),
+        project_name=proposal['project_name'],
+        location=proposal['location'],
+        proposal_date=datetime.strptime(proposal['proposal_date'], "%Y-%m-%d"),
+        total_budget=proposal['total_budget'],
+        latitude=proposal['latitude'],
+        longitude=proposal['longitude'],
+        cost=proposal['cost'],
+        timespan=proposal['timespan'],
+        goal=proposal['goal'],
+        is_accepted=False,
+    ).save()
 
-    constraints = json.load(open('api/seed/data/constraints.csv'))
-    Constraint.objects.all().delete()
-    for constraint in constraints:
-        Constraint(
-            constraint_tyoe=constraint['constraint_type'],
-            code=constraint['code'],
-            max_limit=constraint['max_limit'],
-        ).save()
+constraints = pd.read_csv(open('api/seed/data/constraints.csv'))
+Constraint.objects.all().delete()
+for i, constraint in constraints.iterrows():
+    Constraint(
+        constraint_tyoe=constraint['constraint_type'],
+        code=constraint['code'],
+        max_limit=constraint['max_limit'],
+    ).save()
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects', views.get_all_projects),
-    path('projects/<int:project_id>', views.get_project_detail_by_id),
+    path('projects', views.get_all_or_save_projects),
+    path('projects/<str:project_id>', views.get_project_detail_or_delete_by_id),
     path('projects/filter', views.get_filtered_projects),
 ]
