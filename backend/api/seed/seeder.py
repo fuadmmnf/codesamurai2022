40106f384cddabee9608f67f26ff82f10ba927c8
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-12-20 20:42:28 +0600

    added seeders

--- backend/api/seed/seeder.py
+++ backend/api/seed/seeder.py
@@ -1,18 +1,81 @@
 import json
-from api.models import Project
+from api.models import *
 from datetime import datetime
 
-projects = json.load(open('api/seed/projects.json'))
+roles = json.load(open('api/seed/data/user_types.json'))
+UserType.objects.all().delete()
+for role in roles:
+    UserType(
+        code=role['code'],
+        committee=role['committee'],
+        description=role['description'],
+    ).save()
+
+agencies = json.load(open('api/seed/data/agencies.json'))
+Agency.objects.all().delete()
+for agency in agencies:
+    Agency(
+        code=agency['code'],
+        name=agency['name'],
+        type=agency['type'],
+        description=agency['description'],
+    ).save()
+
+projects = json.load(open('api/seed/data/projects.json'))
 Project.objects.all().delete()
 for project in projects:
     Project(
+        project_id=project['project_id'],
+        exec=Agency.objects.get(code__exact=project['exec']),
         project_name=project['project_name'],
-        category=project['category'],
-        affiliated_agency=project['affiliated_agency'],
-        description=project['description'],
-        project_start_time=datetime.strptime(project['project_start_time'], "%Y-%m-%d"),
-        project_completion_time=datetime.strptime(project['project_completion_time'], "%Y-%m-%d"),
+        location=project['location'],
+        start_date=datetime.strptime(project['start_date'], "%Y-%m-%d"),
         total_budget=project['total_budget'],
-        completion_percentage=project['completion_percentage'],
-        location_coordinates=project['location_coordinates'],
+        latitute=project['latitute'],
+        longitude=project['longitude'],
+        cost=project['cost'],
+        timespan=project['timespan'],
+        goal=project['goal'],
+        completion=project['completion'],
+        actual_cost=project['actual_cost'],
+    ).save()
+
+components = json.load(open('api/seed/data/components.json'))
+Component.objects.all().delete()
+for component in components:
+    Component(
+        component_id=component['component_id'],
+        project_id=Project.objects.get(project_id__exact=component['project_id']),
+        execution_agency=Agency.objects.get(code__exact=component['execution_agency']),
+        component_type=component['component_type'],
+        depends_on=None if len(component['depends_on']) < 1 else Component.objects.get(
+            component_id__exact=component['depends_on']),
+        budget_ratio=component['budget_ratio'],
+    ).save()
+
+proposals = json.load(open('api/seed/data/proposals.json'))
+for proposal in proposals:
+    Project(
+        project_id=proposal['project_id'],
+        exec=Agency.objects.get(code__exact=proposal['exec']),
+        project_name=proposal['project_name'],
+        location=proposal['location'],
+        proposal_date=datetime.strptime(proposal['proposal_date'], "%Y-%m-%d"),
+        total_budget=proposal['total_budget'],
+        latitute=proposal['latitute'],
+        longitude=proposal['longitude'],
+        cost=proposal['cost'],
+        timespan=proposal['timespan'],
+        goal=proposal['goal'],
+        is_accepted=False,
+    ).save()
+
+
+constraints = json.load(open('api/seed/data/constraints.json'))
+Constraint.objects.all().delete()
+for constraint in constraints:
+    Constraint(
+        constraint_tyoe=constraint['constraint_type'],
+        code=constraint['code'],
+        max_limit=constraint['max_limit'],
     ).save()
