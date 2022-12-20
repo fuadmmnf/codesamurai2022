ff0796c910152024c018dc54ff8f09076fd21fcc
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-12-21 01:16:36 +0600

    added gantt to proposals

--- clientapp/src/pages/ExecutingAgenices/proposals/Detail.vue
+++ clientapp/src/pages/ExecutingAgenices/proposals/Detail.vue
@@ -1,58 +1,92 @@
 <template>
   <div class="q-pa-md">
     <div class="text-h6" style="margin-left: 17px; margin-bottom: 15px;">Proposal Name</div>
-    <q-markup-table class="myTable">
-      <tbody>
-      <tr>
-        <th class="text-left ">Location</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Latitude</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Longitude</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Executing Company</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Cost</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Timepsan</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Proposal Date</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Completion</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Goal</th>
-        <td class="text-left">159</td>
-      </tr>
-      <tr>
-        <th class="text-left ">Actions</th>
-        <td class="text-left">
-          <q-btn @click="$router.push('/exec/proposals/update')">Update</q-btn>
-        </td>
-      </tr>
-      </tbody>
-    </q-markup-table>
+    <q-card>
+      <q-tabs
+        v-model="tab"
+        active-color="primary"
+        align="justify"
+        class="text-grey"
+        dense
+        indicator-color="primary"
+      >
+        <q-tab label="Details" name="details"/>
+        <q-tab label="Gantt Chart" name="gantt"/>
+      </q-tabs>
+
+      <q-separator/>
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="details">
+          <q-markup-table class="myTable">
+            <tbody>
+            <tr>
+              <th class="text-left ">Location</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Latitude</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Longitude</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Executing Company</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Cost</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Timepsan</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Proposal Date</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Completion</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Goal</th>
+              <td class="text-left">159</td>
+            </tr>
+            <tr>
+              <th class="text-left ">Actions</th>
+              <td class="text-left">
+                <q-btn @click="$router.push('/exec/proposals/update')">Update</q-btn>
+              </td>
+            </tr>
+            </tbody>
+          </q-markup-table>
+        </q-tab-panel>
+        <q-tab-panel name="gantt">
+          <div style="height: 70vh">
+            <Gantt></Gantt>
+          </div>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
   </div>
 </template>
 
 <script>
+import Gantt from "components/gantt.vue";
 export default {
-  name: "ExecProposalDetails"
+  name: "ExecProposalDetails",
+  components: {
+    Gantt
+  },
+  data() {
+    return{
+      tab: 'details'
+    }
+  }
 }
 </script>
 
