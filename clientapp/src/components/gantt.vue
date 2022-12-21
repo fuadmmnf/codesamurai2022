ffae7c2b1e53ef590b4f8fb11793bbd2f98a2a76
RadowanMahmud <bsse1002@iit.du.ac.bd>
2022-12-21 09:42:29 +0600

    gaan chart basic layout prepared

--- clientapp/src/components/gantt.vue
+++ clientapp/src/components/gantt.vue
@@ -1,5 +1,5 @@
 <template>
-      <div style="height: 100vh; width: 100%;" ref="ganttContainer"></div>
+  <div style="height: 100vh; width: 100%;" ref="ganttContainer"></div>
 </template>
 
 <script>
@@ -14,36 +14,45 @@ export default {
       required: true
     },
   },
-  data () {
+  data() {
     return {
       tasks: {
         data: [
-          {id: 1, text: 'Task #1', start_date: '2020-01-17', duration: 3, progress: 0.6},
-          {id: 2, text: 'Task #2', start_date: '2020-01-20', duration: 3, progress: 0.4}
-        ],
-        links: [
-          {id: 1, source: 1, target: 2, type: '0'}
+          // {id: 1, text: 'Task #1', start_date: '2020-01-17', duration: 3, progress: 0.6},
+          // {id: 2, text: 'Task #2', start_date: '2020-01-20', duration: 3, progress: 0.4}
         ]
       },
     }
   },
   mounted: function () {
-    api.get(`projects/${this.project_id}/components`).then((response)=>{
+    api.get(`projects/${this.project_id}/components`).then((response) => {
       console.log(response.data.data)
+      response.data.data.forEach((element) => {
+        let temp = {
+          id: element.id,
+          text: element.component_id,
+          start_date: '2020-01-17',
+          duration: 3,
+          progress: 0.0
+        }
+        this.tasks.data.push(temp)
+        gantt.config.date_format = "%Y-%m-%d";
+        gantt.init(this.$refs.ganttContainer);
+        gantt.parse(this.tasks);
+      })
     })
-    gantt.config.date_format = "%Y-%m-%d";
-    gantt.init(this.$refs.ganttContainer);
-    gantt.parse(this.tasks);
   }
 }
 </script>
 
 <style>
 @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+
 .container {
   height: 100vh;
   width: 100%;
 }
+
 .left-container {
   overflow: hidden;
   position: relative;
